28894a000b39f83fae8dbd7c3e68a989ea60c381
Tirtho saha <220150.cse@student.just.edu.bd>
2026-06-10 10:09:19 +0600

    d added

---
+++ cpp/cpp/module_1_contest_3/d.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+
+        int k;
+        cin >> k;
+
+        int i = 1, count = 0;
+
+        while (true) {
+
+            if (i % 3 != 0 && i % 10 != 3) {
+                count++;
+            }
+
+            if (count == k) {
+                cout << i << endl;
+                break;
+            }
+
+            i++;
+        }
+    }
+
+    return 0;
+}
