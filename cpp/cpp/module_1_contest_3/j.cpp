38e7babacadc691df5319c1d23746061f2e66c43
Tirtho saha <220150.cse@student.just.edu.bd>
2026-06-11 11:29:18 +0600

    i j added successfully

---
+++ cpp/cpp/module_1_contest_3/j.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    while(t--){
+        int n,x;
+        cin>>n>>x;
+        if(n>x) cout<<"NO"<<endl;
+        else cout<<"YES"<<endl;
+    }
+
+
+    return 0;
+}
