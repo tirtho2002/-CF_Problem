0896b57f503711df54227f5dd009388b28b9fed9
Tirtho saha <220150.cse@student.just.edu.bd>
2026-06-11 10:32:23 +0600

    f added

---
+++ cpp/cpp/module_1_contest_3/f.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    cout<<"L";
+    while (t--)
+    {
+    cout<<"o";    
+    }
+    cout<<"ng";
+    
+
+
+
+    return 0;
+}
