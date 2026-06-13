cb6a54aae7fa463053e93546da8dab09bb242b31
Tirtho saha <220150.cse@student.just.edu.bd>
2026-06-13 22:42:22 +0600

    ghgh

--- cpp/cpp/module_1_contest_3/A - B.cpp
+++ cpp/cpp/module_1_contest_3/A - B.cpp
@@ -2,10 +2,16 @@
 using namespace std;
 
 int main() {
-    int x,y;
-    cin>>x>>y;
+int t;
+cin>>t;
+while(t--){
+    long long n,k,temp;
 
-    cout<<(x*6)+y<<endl;
+    cin>>n>>k;
+    temp= (n / (k + 1)) + (n % (k + 1));
+    cout<<temp<<endl;
+
+}
 
     return 0;
 }
