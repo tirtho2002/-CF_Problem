6eab45779a19b4aea296aeb16d2575b3158c1dfd
tirtho <220150.cse@student.just.edu.bd>
2026-06-18 12:38:46 +0600

    dfv

---
+++ cpp/cpp/module_1_contest_3/t.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+  while(t--){
+   int a;
+   cin>>a;
+   if(a>1){
+    if(a%2==0){
+        cout<<"YES"<<endl;
+
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+   }
+   else{
+    cout<<"YES"<<endl;
+   }
+
+
+  }
+
+    return 0;
+}
