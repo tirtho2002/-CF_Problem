0e0f2ebcbfa3dabbfb47fe87d78e57fad55f84d0
tirtho <220150.cse@student.just.edu.bd>
2026-06-20 23:48:24 +0600

    icpc balloons

---
+++ icpc_balloons.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    while(t--){
+
+        int size;
+        cin>>size;
+        string arr;
+        cin>>arr;
+        set <char> s;
+        for(char a:arr){
+            s.insert(a);
+        }
+        cout<<(s.size()*2)+(size-s.size())<<endl;
+
+    }
+
+
+    return 0;
+}
